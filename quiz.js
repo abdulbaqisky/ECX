acf8d411b14d474e6d3aa1502bf9160f2ddc62c1
“[Adekomi <adekomiabdulbaqi3@gmail.com>
2020-02-28 17:04:06 +0100

    my very first rubbish

---
+++ quiz.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+var myPassword = '/adeleye9932';
+
+
+
+const salt = bcrypt.genSaltSync(saltRounds);
+const hash = bcrypt.hashSync(myPassword, salt);
+
+bcrypt.hashSync(myPassword, hash); // true
+
+console.log(hash);
+
+bcrypt.compare(myPassword, hash, function(err, result) {
+    // result == true
+});
+
+if (myPassword == 'jbsuisdn'){
+    console.log("Yeah")
+}else{
+    console.log(myPassword);
+}
+
+
